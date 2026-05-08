63236a6105819c29799dca1fc7381848afe5c282
Ranjitha051 <ranjithayashu05@gmail.com>
2026-05-08 10:28:35 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,66 +1,99 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main() {
-    int arr[100], n, i, pos, value, choice, found;
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-    printf("Enter elements:\n");
-    for(i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char c) {
+    if (top >= MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
     }
-    do {
-        printf("\n--- MENU ---\n");
-        printf("1. Traversal\n2. Insertion\n3. Deletion\n4. Searching\n5. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        switch(choice) {
-            case 1: // Traversal
-                printf("Array elements are: ");
-                for(i = 0; i < n; i++) {
-                    printf("%d ", arr[i]);
-                }
-                break;
+    stack[++top] = c;
+}
 
-            case 2: // Insertion
-                printf("Enter position and value: ");
-                scanf("%d %d", &pos, &value);
+char pop() {
+    if (top < 0) {
+        return '\0';
+    }
+    return stack[top--];
+}
 
-                for(i = n; i >= pos; i--) {
-                    arr[i] = arr[i - 1];
-                }
-                arr[pos - 1] = value;
-                n++;
-                break;
+char peek() {
+    if (top < 0) {
+        return '\0';
+    }
+    return stack[top];
+}
 
-            case 3: // Deletion
-                printf("Enter position to delete: ");
-                scanf("%d", &pos);
-                for(i = pos - 1; i < n - 1; i++) {
-                    arr[i] = arr[i + 1];
-                }
-                n--;
-                break;
-            case 4: // Searching
-                printf("Enter element to search: ");
-                scanf("%d", &value);
-                found = 0;
-                for(i = 0; i < n; i++) {
-                    if(arr[i] == value) {
-                        printf("Element found at position %d\n", i + 1);
-                        found = 1;
-                        break;
-                    }
-                }
-
-                if(found == 0)
-                    printf("Element not found\n");
-                break;
-            case 5:
-                printf("Exiting...\n");
-                break;
-            default:
-                printf("Invalid choice!\n");
+int precedence(char op) {
+    switch (op) {
+        case '+':
+        case '-':
+            return 1;
+        case '*':
+        case '/':
+            return 2;
+        case '^':
+            return 3;
+        default:
+            return 0;
+    }
+}
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
+}
+void infixToPostfix(char *infix, char *postfix) {
+    int i = 0, j = 0;
+    char c;
+    while ((c = infix[i++]) != '\0') {
+        // If operand, add to output
+        if (isalnum(c)) {
+            postfix[j++] = c;
         }
-    } while(choice != 5);
+        // If '(', push to stack
+        else if (c == '(') {
+            push(c);
+        }
+        // If ')', pop until '(' is found
+        else if (c == ')') {
+            while (top >= 0 && peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop(); // Remove '(' from stack
+        }
+        // If operator
+        else if (isOperator(c)) {
+            // Handle right-associativity for '^'
+            if (c == '^') {
+                while (top >= 0 && precedence(peek()) > precedence(c)) {
+                    postfix[j++] = pop();
+                }
+            } else {
+                while (top >= 0 && precedence(peek()) >= precedence(c)) {
+                    postfix[j++] = pop();
+                }
+            }
+            push(c);
+        }
+    }
+    // Pop remaining operators
+    while (top >= 0) {
+        postfix[j++] = pop();
+    }
+    postfix[j] = '\0';
+}
+int main() {
+    char infix[MAX], postfix[MAX];
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+    infixToPostfix(infix, postfix);
+    printf("Postfix expression: %s\n", postfix);
     return 0;
 }
+
+
